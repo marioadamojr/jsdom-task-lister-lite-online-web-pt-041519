db9e16682b5b7c651e735e43230418688af88b0f
Mario Adamo Jr <marioadamojr@gmail.com>
2019-12-17 14:39:36 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ document.addEventListener("DOMContentLoaded", () => {
   document.querySelector("#create-task-form").addEventListener("submit", function(event) {
     event.preventDefault()
     var e = document.getElementById("new-task-description").value
-    var t = document.getElementById("tasks")
-    t.innerHTML += e
+    var list = document.getElementById("tasks")
+    var entry = document.createElement('li')
+    entry.appendChild(document.createTextNode(e))
+    list.appendChild(entry)
   })
 });

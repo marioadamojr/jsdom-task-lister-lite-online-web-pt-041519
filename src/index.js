6ca46bd27b1000b69787d930e2bc8fc21b47624d
Mario Adamo Jr <marioadamojr@gmail.com>
2019-12-17 14:34:47 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-document.addEventListener("DOMContentLoaded", () => {
-  document.querySelector("#create-task-form").addEventListener("submit", function(event) {
-    event.preventDefault()
-    var e = document.getElementById("new-task-description").value
-    var t = document.getElementById("tasks")
-    t.innerHTML += e
-  })
-});
+// document.addEventListener("DOMContentLoaded", () => {
+//   document.querySelector("#create-task-form").addEventListener("submit", function(event) {
+//     event.preventDefault()
+//     var e = document.getElementById("new-task-description").value
+//     var t = document.getElementById("tasks")
+//     t.innerHTML += e
+//   })
+// });

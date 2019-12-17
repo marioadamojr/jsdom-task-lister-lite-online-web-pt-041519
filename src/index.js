8e058f5f96a58e18eedf1bc7567d444758cf42b4
Mario Adamo Jr <marioadamojr@gmail.com>
2019-12-17 14:43:12 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector("#create-task-form").addEventListener("submit", function(event) {
+    event.preventDefault()
+    var e = document.getElementById("new-task-description").value
+    var list = document.getElementById("tasks")
+    var entry = document.createElement('li')
+    entry.appendChild(document.createTextNode(e))
+    list.appendChild(entry)
+  })
 });
